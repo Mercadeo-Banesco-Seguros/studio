7f18187e9174cec6b8d58788b03f3fb43ea09466
Rommer Alejandro Duque Andrade <rduque@banescoseguros.com>
2025-12-03 21:07:31 +0000

    aumenta el tamaño de la imagen de nuestros pilares solamente para que se

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -416,7 +416,7 @@ export default function DashboardPage() {
                 </div>
               </div>
               
-              <div className="relative h-96 w-full">
+              <div className="relative h-[30rem] w-full group">
                 {aboutContent[activeAboutView].image && (
                   <Image
                     src={aboutContent[activeAboutView].image!}
@@ -424,6 +424,7 @@ export default function DashboardPage() {
                     layout="fill"
                     objectFit="contain"
                     data-ai-hint={aboutContent[activeAboutView]['data-ai-hint']}
+                    className="transition-transform duration-500 ease-in-out group-hover:scale-110"
                   />
                 )}
               </div>
@@ -755,7 +756,7 @@ export default function DashboardPage() {
         <section id="espacio-ejecutivo" className="scroll-mt-20 w-full mt-24">
           <Card className="relative w-full overflow-hidden rounded-none bg-foreground text-primary-foreground shadow-2xl min-h-[400px] flex flex-col justify-center items-center text-center p-8 md:p-12 group">
               <Image
-                  src="https://images.unsplash.com/photo-1610374792793-f016b77ca51a?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=M3w3NDE5ODJ8MHwxfHNlYXJjaHw2fHxleGVjdXRpdmV8ZW58MHx8fHwxNzU2MTM2NDg3fDA&ixlib=rb-4.1.0&q=80&w=1080"
+                  src="https://images.unsplash.com/photo-1610374792793-f016b77ca51a?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=M3w3NDE5ODJ8MHwxfHNlYXJjaHw2fHxleGVjdXRpdmV8ZW58MHx8fHwxNzU2MTM2NDg3fDA&ixlib-rb-4.1.0&q=80&w=1080"
                   alt="Equipo ejecutivo en reunión"
                   layout="fill"
                   objectFit="cover"
