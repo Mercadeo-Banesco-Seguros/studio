9212382812b46ccd90b9f0bff514dfd3be412751
Rommer Alejandro Duque Andrade <rduque@banescoseguros.com>
2025-12-10 15:07:20 +0000

    recuerda que la tarjeta de visita nuestra academia banesco seguros ocupa

--- src/app/dashboard/cursos/page.tsx
+++ src/app/dashboard/cursos/page.tsx
@@ -166,7 +166,7 @@ export default function CursosPage() {
                       <h1 className="text-4xl md:text-5xl font-extrabold text-white tracking-tight">ADN Banesco<br/>Seguros</h1>
                        <p className="mt-4 max-w-xl text-primary-foreground/80">Conoce los principios que nos guían y forman la base de nuestra organización.</p>
                       <div className="pt-8">
-                        <Button asChild className="text-xs font-light bg-primary text-primary-foreground hover:bg-primary/90">
+                        <Button asChild className="text-xs font-light bg-white/20 text-white hover:bg-white/30 backdrop-blur-sm">
                           <Link href="#">
                             Conocer más
                           </Link>
@@ -212,7 +212,7 @@ export default function CursosPage() {
                                 <h2 className="text-2xl font-bold text-white">Soluciones y Cobertura</h2>
                             </div>
                             <div className="mt-auto">
-                                <Button asChild className="text-xs font-light bg-white/20 text-white hover:bg-white/30 backdrop-blur-sm">
+                                <Button asChild className="text-xs font-light bg-primary text-primary-foreground hover:bg-primary/90">
                                     <Link href="#">Conocer más</Link>
                                 </Button>
                             </div>
@@ -375,7 +375,7 @@ export default function CursosPage() {
         <div className="max-w-7xl mx-auto p-4 sm:p-8 space-y-12 mt-12">
             <section>
               <div className="flex gap-6">
-                <div className="w-[50%]">
+                 <div className="w-[70%]">
                   <Card className="relative rounded-2xl shadow-lg overflow-hidden bg-card min-h-[400px] flex flex-col justify-end text-left">
                     <Image 
                       src="https://www.shutterstock.com/image-photo/concentrated-young-female-student-engaged-600nw-2458397479.jpg"
@@ -398,7 +398,7 @@ export default function CursosPage() {
                     </div>
                   </Card>
                 </div>
-                 <div className="w-[50%]">
+                 <div className="w-[30%]">
                   <Card className="relative rounded-2xl shadow-lg overflow-hidden bg-card min-h-[400px] flex flex-col justify-end text-left">
                     <Image 
                       src="https://www.ceupe.com/images/easyblog_articles/3074/b2ap3_amp_formacion-in-company-vs-formacion-externa.jpg"
