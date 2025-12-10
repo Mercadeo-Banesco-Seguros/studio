94d752085fb77ee87cc4a5767144e7087fd71048
Rommer Alejandro Duque Andrade <rduque@banescoseguros.com>
2025-12-10 21:35:23 +0000

    Please update the image here to the newly chosen ones (_for element img_

--- src/app/dashboard/cursos/page.tsx
+++ src/app/dashboard/cursos/page.tsx
@@ -156,11 +156,11 @@ export default function CursosPage() {
                 <div className="w-[70%]">
                   <Card className="relative rounded-2xl shadow-lg overflow-hidden bg-primary text-primary-foreground min-h-[600px] flex flex-col justify-end text-left">
                     <Image
-                      src="https://images.unsplash.com/photo-1542744173-8e7e53415bb0?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=M3w3NDE5ODJ8MHwxfHNlYXJjaHwyfHxidXNpbmVzcyUyMHRlYW18ZW58MHx8fHwxNzY1MzgxMjYyfDA&ixlib=rb-4.1.0&q=80&w=1080"
+                      src="https://images.unsplash.com/photo-1681127910416-f18e60b0ba1a?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=M3w3NDE5ODJ8MHwxfHNlYXJjaHwxMXx8RE5BfGVufDB8fHx8MTc2NTM4MDMwMnww&ixlib=rb-4.1.0&q=80&w=1080"
                       alt="ADN Banesco Seguros"
                       layout="fill"
                       objectFit="cover"
-                      data-ai-hint="business team"
+                      data-ai-hint="DNA illustration"
                       className="brightness-50"
                     />
                     <div className="relative z-10 p-8 md:p-12">
