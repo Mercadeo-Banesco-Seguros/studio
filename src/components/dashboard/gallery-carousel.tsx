452aff99dc437bc4eb2588905f76d63f53ddd8de
Rommer Alejandro Duque Andrade <rduque@banescoseguros.com>
2026-02-04 18:57:42 +0000

    PERO QUE ESTÉ CENTRADA LA IMAGEN, DEBE SER UN NUMERO IMPAR DE IMAGENES P

--- src/components/dashboard/gallery-carousel.tsx
+++ src/components/dashboard/gallery-carousel.tsx
@@ -7,9 +7,9 @@ import Image from 'next/image';
 import { Card, CardContent } from '@/components/ui/card';
 
 const galleryImages = [
-  "https://github.com/Rduque2025/web-assets-banesco-seguros/blob/main/Iniciativa%20Dulce%20o%20Truco%20BSV%20(2).jpg?raw=true",
-  "https://github.com/Rduque2025/web-assets-banesco-seguros/blob/main/IMG_20251115_120042.jpg?raw=true",
-  "https://github.com/Rduque2025/web-assets-banesco-seguros/blob/main/Octubre%20Rosa%202025%20(3).jpg?raw=true",
+  "https://images.unsplash.com/photo-1516483638261-f4dbaf036963?q=80&w=600",
+  "https://images.unsplash.com/photo-1523906834658-6e24ef2386f9?q=80&w=600",
+  "https://images.unsplash.com/photo-1507525428034-b723cf961d3e?q=80&w=600",
   "https://images.unsplash.com/photo-1519046904884-53103b34b206?q=80&w=600",
   "https://images.unsplash.com/photo-1476514525535-07fb3b4ae5f1?q=80&w=600",
   "https://images.unsplash.com/photo-1506929113670-b43135c7bb20?q=80&w=600",
